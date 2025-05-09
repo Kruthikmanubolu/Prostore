4d1212c712949a8f79501358ea2859bf3f7e1d85
“Kruthikmanubolu” <kruthikmanubolu@gmail.com>
2025-05-08 19:42:24 -0700

    inital deployment-2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
+  // Extends base Next.js ESLint configs
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+
+  // Define custom rules to disable
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off', // Disable 'no-explicit-any' rule
+    '@typescript-eslint/no-unused-vars': 'off', // Disable 'no-unused-vars' rule
+    '@typescript-eslint/no-unused-expressions': 'off', // Disable 'no-unused-expressions' rule
+    '@typescript-eslint/no-this-alias': 'off', // Disable 'no-this-alias' rule
+    '@typescript-eslint/no-require-imports': 'off', // Disable 'no-require-imports' rule
+  },
+};
 
 export default eslintConfig;
