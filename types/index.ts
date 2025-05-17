de95b4e5b68ff2a6cc5a8c30513fe34e6fd5fda2
“Kruthikmanubolu” <kruthikmanubolu@gmail.com>
2025-05-17 12:25:05 -0700

    Add to cart functionality_Fix

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import {z} from 'zod'
-import { insertProductSchema, inserCartSchema, cartItemSchema } from '@/lib/validators';
+import { insertProductSchema, insertCartSchema, cartItemSchema } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
@@ -7,5 +7,5 @@ export type Product = z.infer<typeof insertProductSchema> & {
     createdAt: Date;
 }
 
-export type Cart = z.infer<typeof inserCartSchema>;
+export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
