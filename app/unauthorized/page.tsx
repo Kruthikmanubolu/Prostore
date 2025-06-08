a1bb23ec7974e9e300e9dc4b53cd2604731bff65
“Kruthikmanubolu” <kruthikmanubolu@gmail.com>
2025-06-08 12:46:24 -0700

    unauthorized page bg change

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 const Unauthorized = () => {
 
     return (
-        <div className="container mx-auto flex flex-col items-center justify-center space-y-4 h-[calc(100vh-200px)]">
+        <div className="container mx-auto flex min-h-screen flex-col bg-gradient-to-b from-[#f4f4f2] via-[#eae8e1] to-[#dcd9d3] text-neutral-900 dark:bg-gradient-to-b dark:from-[#0f0f0f] dark:via-[#1a1a1a] dark:to-black dark:text-white items-center justify-center space-y-4 h-[calc(100vh-200px)]">
             <h1 className="h1-bold text-4xl">Unauthorized Access</h1>
             <p className="text-muted-foreground">You do not have permission to access this page</p>
             <Button asChild>
